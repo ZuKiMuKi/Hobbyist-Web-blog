6eaca652fa8f31ba8d7f2a1b34bd7ca79eeee082
ZuKiMuKi <95204259+ZuKiMuKi@users.noreply.github.com>
2021-11-29 19:01:57 +0700

    updated main page

--- js/main.js
+++ js/main.js
@@ -8,5 +8,23 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     })
     /* owl carousel */
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        nav: false,
+        dots: false,
+        responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+    });
 });
